6628d3bcd916dc1a75ce6ea84b8f4f4b6c5323e2
Alexandra Grace Roberts <agr78@cornell.edu>
2023-12-27 15:31:40 -0500

    Copied `parse_QSM_input` from `MEDI` repo

--- code/dependencies/MEDI_functions/parse_QSM_input.m
+++ code/dependencies/MEDI_functions/parse_QSM_input.m
@@ -2,28 +2,30 @@
 %   Created by Tian Liu on 2011.03.16
 %   Modified by Tian Liu and Shuai Wang on 2011.03.15
 %   Last modified by Tian Liu on 2013.07.24
+%   Last modified by Alexandra Roberts on 2023.27.12
 
-function [lam, iFreq, RDF, N_std, iMag, Mask, matrix_size, voxel_size, ...
-    delta_TE, CF, B0_dir, merit, smv, radius, data_weighting, gradient_weighting, ... 
-    Debug_Mode, lam_CSF, Mask_CSF, dip_filt, opts] = parse_QSM_input(varargin)
+function [lam, iFreq, RDF, N_std, iMag, Mask, matrix_size, matrix_size0, voxel_size, ...
+    delta_TE, CF, B0_dir, merit, smv, msmv, radius, data_weighting, gradient_weighting, ... 
+    Debug_Mode, lam_CSF, Mask_CSF, opts] = parse_QSM_input(varargin)
 
 merit = 1;
 smv = 1;
+msmv = 0;
 lam = 1000;
 radius = 5;
 data_weighting = 1;
 gradient_weighting = 1;
 pad = 0;
+matrix_size0 = 0;
 Debug_Mode = 'NoDebug';
 % CSF regularization
 lam_CSF = 100;
 
-
-opts=struct;
+opts = struct;
 opts.filename = ['RDF.mat'];
 opts.solver='gaussnewton';
 opts.percentage = 0.9;
-opts.linear = false;
+opts.linear = false;    
 opts.Lalpha = 100;
 opts.Lkappa = 0.1;
 opts.Lradius = 1;
@@ -33,33 +35,47 @@ opts.cg_max_iter = 100;
 opts.cg_tol = 0.01;
 opts.max_iter = 10;
 opts.tol_norm_ratio = 0.1;
-opts.regs=struct;
-opts.smv_shrink_mask=1;
-opts.resultsdir=fullfile(pwd, 'results');
+opts.regs = struct;
+opts.smv_shrink_mask = 1;
+opts.resultsdir = fullfile(pwd, 'results');
 
 if size(varargin,2)>0
-    for k=1:size(varargin,2)
+    for k = 1:size(varargin,2)
         if strcmpi(varargin{k},'lambda')
-            lam=varargin{k+1};
+            lam = varargin{k+1};
         end
         if strcmpi(varargin{k},'data_weighting')
-            data_weighting=varargin{k+1};
+            data_weighting = varargin{k+1};
         end
         if strcmpi(varargin{k},'gradient_weighting')
-            gradient_weighting=varargin{k+1};
+            gradient_weighting = varargin{k+1};
         end
         if strcmpi(varargin{k},'merit')
-            merit=1;
+            merit = 1;
         end
         if strcmpi(varargin{k},'nomerit')
-            merit=0;
+            merit = 0;
         end
         if strcmpi(varargin{k},'smv')
             smv = 1;
-            radius = varargin{k+1};
+            if length(varargin)>k
+                radius = varargin{k+1};
+                disp(strcat('Using SMV with radius',{' '},string(radius)))
+            end
         end
         if strcmpi(varargin{k},'nosmv')
             smv = 0;
+        end
+        if strcmpi(varargin{k},'msmv')
+            if smv == 1
+                disp('Cannot specify both SMV and mSMV filtering, using mSMV filtering')
+            end
+            msmv = 1;
+            smv = 0;
+            if length(varargin)>k
+                radius = varargin{k+1};
+                disp(strcat('Using mSMV with radius',{' '},string(radius)))
+            end
         end
         if strcmpi(varargin{k},'zeropad')
             pad = varargin{k+1};
@@ -70,34 +86,28 @@ if size(varargin,2)>0
         if strcmpi(varargin{k},'lambda_CSF')
             lam_CSF = varargin{k+1};
         end
-        fn=fieldnames(opts);
-        for j=1:length(fn)
+        fn = fieldnames(opts);
+        for j = 1:length(fn)
             if strcmpi(varargin{k},fn{j})
                 opts.(fn{j}) = varargin{k+1};
             end
         end
-        % added by Ali 07.22.2022
-        if strcmpi(varargin{k},'dipole_filter')
-            dip_filt=varargin{k+1};
-        end
-        % AGR
     end
 end
 
-load(opts.filename,'iFreq','RDF', 'N_std', 'iMag', 'Mask', 'matrix_size', 'voxel_size', 'delta_TE' ,'CF', 'B0_dir');
-
+load(opts.filename,'iFreq','RDF', 'N_std', 'iMag', 'Mask', 'matrix_size', 'voxel_size', 'delta_TE' ,'CF', 'B0_dir', 'R2s');
 opts.matrix_size = matrix_size;
 opts.voxel_size = voxel_size;
-opts.lam_CSF=lam_CSF;
+opts.lam_CSF = lam_CSF;
 opts.Mask = Mask;
-
+opts.R2s = R2s;
 % CSF regularization
 if ismember('Mask_CSF', who('-file', opts.filename))
     Mask_CSF = logical(getfield(load(opts.filename, 'Mask_CSF'), 'Mask_CSF'));
 else
     Mask_CSF = [];
 end
-opts.Mask_CSF=Mask_CSF;
+opts.Mask_CSF = Mask_CSF;
 
 if ismember('weightS', who('-file', opts.filename))
     opts.weightS = getfield(load(opts.filename, 'weightS'), 'weightS');
@@ -105,17 +115,17 @@ else
     opts.weightS = [];
 end
 
-if exist('delta_TE','var')==0
+if exist('delta_TE','var') == 0
     delta_TE = input('TE spacing = ');
     save(opts.filename,'delta_TE','-append');
 end
 
-if exist('CF','var')==0
+if exist('CF','var') == 0
     CF = input('Central Frequency = ');
     save(opts.filename,'CF','-append');
 end
 
-if exist('B0_dir','var')==0
+if exist('B0_dir','var') == 0
     B0_dir = input('B0 direction = ');
     save(opts.filename,'B0_dir','-append');
 end
@@ -135,6 +145,7 @@ else
 end
 
 if sum(pad(:))
+    matrix_size0 = matrix_size;
     matrix_size = matrix_size + pad;
     iFreq = padarray(iFreq, pad,'post');
     RDF = padarray(RDF, pad,'post');
