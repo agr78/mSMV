b9c14b61ab52778d6ec07ab7d9b1965fd8687a1d
Ali <agr78@cornell.edu>
2025-07-29 13:42:05 -0400

    Removed `radius` displays

--- code/mSMV_functions/vSMV.m
+++ code/mSMV_functions/vSMV.m
@@ -1,26 +1,21 @@
-function RDF = vSMV(RDF,Mask,voxel_size,rmax)
+function [RDF,SphereK] = vSMV(RDF,Mask,voxel_size,rmax)
     matrix_size = size(Mask);
     bg = zeros(matrix_size(1),matrix_size(2),matrix_size(3),rmax);
     bgs = zeros(matrix_size);
     shells = zeros(matrix_size);
+    spheres = zeros(matrix_size);
+    RDFs = zeros(matrix_size);
     for j = 1:rmax
-        j
         if j == 1
             rmax
             shells(:,:,:,j) = MaskErode(Mask,matrix_size,voxel_size,rmax-1);
             bg(:,:,:,j) = SMV(RDF,matrix_size,voxel_size,rmax);
+            spheres(:,:,:,j) = single(sphere_kernel(matrix_size, voxel_size,rmax));
         else
-            disp('For radius')
-            rmax-j+1
-            disp('Compute shell between')
-            rmax-j+1
-            rmax-j
-            if rmax-j == 0
-               shells(:,:,:,j) = Mask-MaskErode(Mask,matrix_size,voxel_size,rmax-j+1);
-            else
-               shells(:,:,:,j) = MaskErode(Mask,matrix_size,voxel_size,rmax-j)-MaskErode(Mask,matrix_size,voxel_size,rmax-j+1);
+              shells(:,:,:,j) = MaskErode(Mask,matrix_size,voxel_size,rmax-j+2)-MaskErode(Mask,matrix_size,voxel_size,rmax-j+1);
+              spheres(:,:,:,j) = spheres(:,:,:,1)-single(sphere_kernel(matrix_size, voxel_size,rmax-j+1));
             end
             bg(:,:,:,j) = SMV(RDF,matrix_size,voxel_size,rmax-j+1);
-        end
+            RDFs(:,:,:,j) = shells(:,:,:,j).*(RDF-bg(:,:,:,j));
     end
-    RDF = Mask.*(RDF-SMV(sum(shells.*bg,4),matrix_size,voxel_size,1));
+    RDF = sum(RDFs.*shells,4);
